664f8bf8417b0fa3afb1badbe198128ad3159fc6
sumeyahussein17 <sumeyahussein21@gmail.com>
2025-04-16 17:10:24 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
 import { image } from "../data/data";
+import image from "../images/your-image.jpg";
 
 function About() {
-  return <div>About</div>;
+  return(
+    <div id="About">
+      <h2>About Me</h2>
+      <p>full struck software engineering</p>
+      <img src={image}alt="I made this" />
+      
+    </div>
+  ) 
 }
 
 export default About;
